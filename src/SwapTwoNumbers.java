332353154483ab4e419bbfd9a963bda415c48c2a
xsone <x.sonechkin@gmail.com>
2024-05-14 13:54:26 -0500

    SwapTwoNumbers

--- src/SwapTwoNumbers.java
+++ src/SwapTwoNumbers.java
@@ -1,5 +1,18 @@
 public class SwapTwoNumbers {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int a =5;
+        int b =12;
+
+        /* Method 1
+        int c =a;
+        a = b;
+        b=c;
+        System.out.println(a+"  "+b); */
+
+        //Method 2
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        System.out.println(a+"  "+b);
     }
 }
