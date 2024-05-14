ecd96e5660471b2d9669f6181674e260a97ea1b3
msonechkin <mark.sonechkin@gmail.com>
2024-05-14 15:46:10 -0500

    SwapTwoNumbers

--- src/ReverseNumber.java
+++ src/ReverseNumber.java
@@ -1,5 +1,17 @@
 public class ReverseNumber {
     public static void main(String[] args) {
-        int n = 285;
+        int n = 12309;
+        int reverse = 0;
+
+        while(n>0){
+           reverse = reverse*10 + n%10;
+           n=n/10;
+        }
+        System.out.println(reverse);
+
+//        StringBuffer sb = new StringBuffer(String.valueOf(n));
+//        System.out.println(sb.reverse());
     }
+
+
 }
